7ccbfcb8fefd69144c51aeb9111fccd1a1a6ec3f
Alexander <admin@AM-PC-2.local>
2014-09-30 18:50:15 +0300

    Initial Commit

---
+++ MultimediaData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultimediaData
+//
+//  Created by Alexander on 30.09.14.
+//  Copyright (c) 2014 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultimediaData/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultimediaData
+//
+//  Created by Alexander on 30.09.14.
+//  Copyright (c) 2014 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
